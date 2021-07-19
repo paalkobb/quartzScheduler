a329f7b46f91674995431d8a16593dd6c9408bfb
Paal Kobbeltvedt <paal.kobbeltvedt@goodtech.no>
2021-07-19 09:18:13 +0200

    First commit

---
+++ src/test/java/com/quartzScheduler/quartzScheduler/QuartzSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quartzScheduler.quartzScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

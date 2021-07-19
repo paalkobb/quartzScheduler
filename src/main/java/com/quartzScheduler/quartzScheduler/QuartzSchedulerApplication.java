931d95cdcfff2559a3ba93a55c5701b64291d21c
Paal Kobbeltvedt <paal.kobbeltvedt@goodtech.no>
2021-07-19 09:33:12 +0200

    Added SampleJob class

--- src/main/java/com/quartzScheduler/quartzScheduler/QuartzSchedulerApplication.java
+++ src/main/java/com/quartzScheduler/quartzScheduler/QuartzSchedulerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuartzSchedulerApplication {
-
+	//	https://www.baeldung.com/spring-quartz-schedule
 	public static void main(String[] args) {
 		SpringApplication.run(QuartzSchedulerApplication.class, args);
 	}
---
+++ src/main/java/com/quartzScheduler/quartzScheduler/model/SampleJob.java
@@ -0,0 +1,14 @@
+package com.quartzScheduler.quartzScheduler.model;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class SampleJob implements Job {
+
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        
+    }
+}
